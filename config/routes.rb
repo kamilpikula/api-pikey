0af1ae25ba4653be87efe114633cf9ecad104ca6
Kamil Pikula <kamil.pikula@proxi.cloud>
2019-08-01 17:09:28 +0200

    change subdomain to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        resources :users
-      end
+  namespace :api do
+    namespace :v1 do
+      resources :users
     end
   end
 end
