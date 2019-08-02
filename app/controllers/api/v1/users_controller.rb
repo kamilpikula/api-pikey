60e8a37e1c1cd90d157c8946e852b6b0545e2ff9
Kamil Pikula <kamil.pikula@proxi.cloud>
2019-08-02 15:58:42 +0200

    fix api users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,10 @@
-module Api:V1
+module Api::V1
   class UsersController < ApiController
   
-  # GET /v1/users
-  def index
-    render json: User.all
+    # GET /v1/users
+    def index
+      render json: User.all
+    end
+
   end
 end
